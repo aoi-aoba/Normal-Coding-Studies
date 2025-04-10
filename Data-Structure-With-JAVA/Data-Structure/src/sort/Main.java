2bf329c13803079b6f0d75aabc4fd5480ac3b9be
aoi-aoba <aoiaobaprod@gmail.com>
2025-04-10 14:44:01 +0900

    버블 정렬 학습 및 구현

--- Data-Structure-With-JAVA/Data-Structure/src/sort/Main.java
+++ Data-Structure-With-JAVA/Data-Structure/src/sort/Main.java
@@ -1,19 +1,21 @@
 package sort;
 
 public class Main {
-    static final int NUM_SCALE = 1000;
-    static final int SIZE = 20;
+    static final int NUM_SCALE = 100000;
+    static final int SIZE = 1000;
     public static void prepare(int[] arr) {
         for (int i = 0; i < arr.length; i++)
             arr[i] = (int) (NUM_SCALE * Math.random());
     }
     public static void main(String[] args) {
-        int[] arr = new int[SIZE];
+        int[] arr= new int[SIZE];
         prepare(arr);
+
         Sort sort = new Sort(arr);
-        sort.selectionSort();
+        // sort.selectionSort();
+        sort.bubbleSort();
 
         arr = sort.getArray();
-        for (int i : arr) System.out.print(i + " ");
+        for(int i : arr) System.out.print(i + " ");
     }
 }
--- Data-Structure-With-JAVA/Data-Structure/src/sort/Sort.java
+++ Data-Structure-With-JAVA/Data-Structure/src/sort/Sort.java
@@ -3,9 +3,8 @@ package sort;
 public class Sort {
     int Array[];
 
-    public Sort(int[] Target) {
-        Array = Target;
-    }
+    public Sort(int[] Target) { Array = Target; }
+    public int[] getArray() { return Array; }
 
     public void selectionSort() {
         int k, tmp;
@@ -24,7 +23,20 @@ public class Sort {
         return largest;
     }
 
-    public int[] getArray() {
-        return Array;
+    public void bubbleSort() {
+        int tmp = 0;
+        boolean swapped;
+        for (int last = Array.length-1; last >= 2; last--) {
+            swapped = false;
+            for (int i = 0; i <= last-1; i++) {
+                if (Array[i] > Array[i+1]) {
+                    tmp = Array[i];
+                    Array[i] = Array[i+1];
+                    Array[i+1] = tmp;
+                    swapped = true;
+                }
+            }
+            if(!swapped) break;
+        }
     }
 }
